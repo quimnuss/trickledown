b4c26186370f7d5fcc62da6ea288abc3f5285e43
Quim Nuss Székely <quimnuss@altaveu>
2024-11-17 23:16:46 +0100

    chore: reset spaces instead of tabs, editor settings reseted

--- scripts/config.gd
+++ scripts/config.gd
@@ -13,38 +13,38 @@ var config = ConfigFile.new()
 signal language_changed
 
 func _ready():
-	var err = config.load(SAVEFILE)
-	if err != OK:
-		config_init()
-	load_config()
-	show_config()
+    var err = config.load(SAVEFILE)
+    if err != OK:
+        config_init()
+    load_config()
+    show_config()
 
 
 func config_init():
-	config.set_value("preferences", "skip_tutorial", false)
-	config.set_value("preferences", "language", "en")
-	config.save(SAVEFILE)
+    config.set_value("preferences", "skip_tutorial", false)
+    config.set_value("preferences", "language", "en")
+    config.save(SAVEFILE)
 
 
 func load_config():
-	skip_tutorial = config.get_value("preferences", "skip_tutorial", skip_tutorial)
-	lang = config.get_value("preferences", "language", lang)
-	TranslationServer.set_locale(lang)
+    skip_tutorial = config.get_value("preferences", "skip_tutorial", skip_tutorial)
+    lang = config.get_value("preferences", "language", lang)
+    TranslationServer.set_locale(lang)
 
 
 func save():
-	config.set_value("preferences", "skip_tutorial", skip_tutorial)
-	config.set_value("preferences", "language", lang)
-	config.save(SAVEFILE)
+    config.set_value("preferences", "skip_tutorial", skip_tutorial)
+    config.set_value("preferences", "language", lang)
+    config.save(SAVEFILE)
 
 
 func show_config():
-	prints('config', SAVEFILE, ':\nskip_tutorial', skip_tutorial, 'language:', lang)
+    prints('config', SAVEFILE, ':\nskip_tutorial', skip_tutorial, 'language:', lang)
 
 
 func change_language(new_language_id : int):
-	lang = langs[new_language_id % len(langs)]
-	prints('Change language to', lang)
-	TranslationServer.set_locale(lang)
-	save()
-	language_changed.emit(lang)
+    lang = langs[new_language_id % len(langs)]
+    prints('Change language to', lang)
+    TranslationServer.set_locale(lang)
+    save()
+    language_changed.emit(lang)
