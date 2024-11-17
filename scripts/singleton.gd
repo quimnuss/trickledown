0ee63b95e00ca43c289a3b8beaed809c6756eaad
Quim Nuss Székely <quimnuss@altaveu>
2024-11-17 23:11:31 +0100

    fix: recover from corruption, disable unimplemented milestones

--- scripts/singleton.gd
+++ scripts/singleton.gd
@@ -5,23 +5,33 @@ var do_reset : bool = true
 enum Scene {SYSTEM, CITY, WORK}
 
 var focused_body : Node2D:
-    set(body):
-        focused_body = body
-        focused_body_changed.emit()
+	set(body):
+		focused_body = body
+		focused_body_changed.emit()
 
 signal focused_body_changed
 
 enum Milestone {
-    LAUNCH,
-    CRASH_HOME,
-    LAUNCH_ATMOSPHERE,
-    ORBIT_ONE,
-    CRASH_STAR,
-    ORBIT_STABLE,
-    CRASH_SATELLITE,
-    ORBIT_STAR,
-    ORBIT_SATELLITE,
-    ESCAPE_VELOCITY,
-    VISIT_3,
-    CRASH_ASTRONAUT,
+	LAUNCH,
+	CRASH_HOME,
+	LAUNCH_ATMOSPHERE,
+	ORBIT_ONE,
+	CRASH_STAR,
+	ORBIT_STABLE,
+	CRASH_SATELLITE,
+	ORBIT_STAR,
+	ORBIT_SATELLITE,
+	ESCAPE_VELOCITY,
+	VISIT_3,
+	CRASH_ASTRONAUT,
 }
+
+var disabled_milestones : Array[String] = [
+	'LAUNCH_ATMOSPHERE',
+	'ORBIT_ONE',
+	'ORBIT_STABLE',
+	'ORBIT_STAR',
+	'ORBIT_SATELLITE',
+	'ESCAPE_VELOCITY',
+	'CRASH_ASTRONAUT',
+]
--- ui/milestone_toast.gd
+++ ui/milestone_toast.gd
@@ -2,32 +2,28 @@ extends RichTextLabel
 
 var completed_milestones : Array[Singleton.Milestone]
 
-var parametrized_text : String
-
 func _ready() -> void:
-    self.modulate.a = 0
+	self.modulate.a = 0
 
-    var current_save : SaveResource = load(Config.save_path)
-    completed_milestones = current_save.milestones.duplicate()
-
-    parametrized_text = self.text
+	var current_save : SaveResource = load(Config.save_path)
+	completed_milestones = current_save.milestones.duplicate()
 
 func fade_in():
-    var tween = create_tween()
-    tween.tween_property(self, 'modulate:a', 1, 1)
+	var tween = create_tween()
+	tween.tween_property(self, 'modulate:a', 1, 1)
 
 func fade_out():
-    var tween = create_tween()
-    tween.tween_property(self, 'modulate:a', 0, 1)
+	var tween = create_tween()
+	tween.tween_property(self, 'modulate:a', 0, 1)
 
 func toast():
-    var tween = create_tween()
-    tween.tween_property(self, 'modulate:a', 1, 1)
-    tween.tween_property(self, 'modulate:a', 0, 1).set_delay(2)
+	var tween = create_tween()
+	tween.tween_property(self, 'modulate:a', 1, 1)
+	tween.tween_property(self, 'modulate:a', 0, 1).set_delay(2)
 
 func _on_milestone_completed(milestone_enum : Singleton.Milestone):
-    if milestone_enum not in completed_milestones:
-        var milestone_name : String = Singleton.Milestone.keys()[milestone_enum]
-        self.text = '[rainbow freq=0.5 sat=0.5][center][wave]%s[/wave][/center][/rainbow]' % tr(milestone_name)
-        toast()
-        completed_milestones.append(milestone_enum)
+	if milestone_enum not in completed_milestones:
+		var milestone_name : String = Singleton.Milestone.keys()[milestone_enum]
+		self.text = '[rainbow freq=0.5 sat=0.5][center][wave]%s[/wave][/center][/rainbow]' % tr(milestone_name)
+		toast()
+		completed_milestones.append(milestone_enum)
