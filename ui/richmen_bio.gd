076ac59735410153d204bb52bf3bc95355f3e3b7
quimnuss <quimnuss@petit>
2024-12-10 09:33:02 +0100

    wip: bio modal scene with a ninepatch

--- ui/richmen_bio.gd
+++ ui/richmen_bio.gd
@@ -1,10 +1,12 @@
 extends Control
 
-@onready var bio_text : RichTextLabel = $VBoxContainer/BioText
+@onready var bio_title : Label = %BioTitle
+@onready var bio_text : RichTextLabel = %BioText
 
 
 func set_bio_text(richmen_name : String, richmen_bio : String):
-    bio_text.text = "[font_size=30][center]%s[/center][/font_size]\n%s" % [richmen_name, richmen_bio]
+    bio_title.text = richmen_name
+    bio_text.text = richmen_bio
 
 func _on_richmen_portrait_toggled(toggled_on: bool) -> void:
     self.visible = toggled_on
